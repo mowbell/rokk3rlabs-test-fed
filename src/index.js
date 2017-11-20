a177c1aeaefe849e55a9beb5f3c0202020bea3da
Mauricio Bello <mow_bell@hotmail.com>
2017-11-19 23:28:20 -0500

    Fix build task issues

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import angular from 'angular';
 import {main} from './app/main';
 import 'angular-ui-router';
 import 'normalize.css';
-import 'font-awesome/css/font-awesome.css';
 import './app/dashboard';
 import routesConfig from './routes';
 
